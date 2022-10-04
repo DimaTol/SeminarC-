cd33c519da50c580b873215160f15fc78c7dfa24
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-04 14:53:59 +0300

    проверяем кварат первого числа

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите перво число: ");
+int firstnumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondnumber = Convert.ToInt32 (Console.ReadLine());
+int square = secondnumber*firstnumber;
+
+if (square == secondnumber*firstnumber)
+{
+    Console.WriteLine ("ДА");
+}
+else
+
+     Console.WriteLine ("НЕТ");
