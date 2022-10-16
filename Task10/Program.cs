cebf83d4aeed2cd95bffe82d15e8046677d4e23b
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-16 10:35:26 +0300

    Задача про метод и бул

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number = 23534;
+if (Check(number))
+{
+    int res = Method(number);
+}
+else {}
+
+bool Check (int num)
+{
+    return number >= 100 & number <= 999; // возвращает true или false 
+    // или так
+    // res = number >= 100 & number <= 999;
+    // return res;
+}
+
+int Method(int num);
+{
+    return num / 10 % 10; // 345
+}
