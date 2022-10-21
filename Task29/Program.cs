c05331e11bd8fdd75e6360ec96eb21e202bb095c
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-21 20:05:47 +0300

    Напишите программу, которая задаёт массив из 8 элементов // и выводит их на экран

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+Array(array);
+
+void Array(int[] numbers)
+ {
+    for(int index = 0; index < numbers.Length; index++)
+     {
+        numbers[index] = new Random().Next(0,8); 
+        Console.Write (numbers[index] + ",");
+        
+     }
+ }
+
