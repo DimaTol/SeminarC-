7db68a939bc7cafdb515590e7832daa355ae77fd
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-21 19:12:34 +0300

    Задача где возводит число A в натуральную степень B

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -9,21 +9,21 @@ Console.Write("Введите число В: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 
 if (numA < 0 || numB < 0)
-Console.Write("Введите положительное число");
+    Console.Write("Введите положительное число");
 
 else
 {
-int Degree(int a, int b)
-{
-    int result = 1;
-    for (int i = 0; i < numB; i++)
+    int Degree(int a, int b)
     {
-        result = result * numA;
-    }
-    return result;
-    
-}
-int degr = Degree (numA,numB);
+        int result = 1;
+        for (int i = 0; i < numB; i++)
+        {
+            result = result * numA;
+        }
+        return result;
 
-Console.WriteLine ($"Число {numA} в степени {numB} -> {degr}");
+    }
+    int degr = Degree(numA, numB);
+
+    Console.WriteLine($"Число {numA} в степени {numB} -> {degr}");
 }
