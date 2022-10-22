447dca29d41071205457212baec13df05b496ca7
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-22 11:26:55 +0300

    Напишите программу, которая задаёт массив из 8 элементов // и выводит их на экран

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,16 +2,46 @@
 // и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-int[] array = new int[8];
-Array(array);
-
-void Array(int[] numbers)
+int[] FillArray(int num)
 {
-    for (int index = 0; index < numbers.Length; index++)
+    int[] array = new int[num];
+    Random rnd = new Random ();
+    for (int i = 0; i < array.Length; i++)
     {
-        numbers[index] = new Random().Next(0, 8);
-        Console.Write(numbers[index] + ",");
+        array[i] = rnd.Next (0,8);
+    }
+    return array;
+}
 
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
     }
 }
 
+int[] myArr = FillArray(8);
+PrintArray(myArr);
+
+
+
+
+
+
+
+
+
+// int[] array = new int[8];
+// Array(array);
+
+// void Array(int[] numbers)
+// {
+//     Random rnd = new Random();
+//     for (int index = 0; index < numbers.Length; index++)
+//     {
+//         numbers[index] = rnd.Next(0, 8);
+//         Console.Write(numbers[index] + ",");
+
+//     }
+// }
