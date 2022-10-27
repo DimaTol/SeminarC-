303fa2a012e9377b5f317da27bb0771ad7bd9c9d
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-27 20:32:53 +0300

    на вход 3 числа и проверяет есть ли такой треугольник

---
+++ Task40/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40: Напишите программу, которая 
+// 1. принимает на вход три числа и 
+// 2. проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle (int numA, int numB, int numC)
+{
+    return (numA < numB + numC && numB < numA + numC && numC < numA + numB);
+}
+bool triangle = Triangle (numberA, numberB, numberC);
+
+if (triangle) Console.WriteLine("Такой треугльник существует");
+else Console.WriteLine ("Такой треугльник не существует");
