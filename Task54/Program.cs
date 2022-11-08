97297eaec4e1c737293d4568b0c1bd2543aff6e2
DimaTol <d.tolkachenko@ya.ru>
2022-11-08 15:21:06 +0300

    Упорядочевание элементов каждой строки двумерного массива

---
+++ Task54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: 
+//1. Задайте двумерный массив. 
+//2. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int row, int columns, int min, int max)
+{
+    var matrix = new int[row, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+return matrix;
+}
+
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+    if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4},");
+    else Console.Write($"{matrix[i, j], 4}");
+    }
+    Console.WriteLine("]");
+    }
+}
+
+void SortingArray(int[,] sortingArray)
+{
+    int temp;
+    int max;
+    for (int i = 0; i < sortingArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < sortingArray.GetLength(1) - 1; j++)
+        {
+            max = j;
+
+            for (int k = j; k < sortingArray.GetLength(1); k++)
+            {
+                if (sortingArray[i, k] > sortingArray[i, max])
+                {
+                    max = k;
+                }
+            }
+            if (max != j)
+            {
+                temp = sortingArray[i, j];
+                sortingArray[i, j] = sortingArray[i, max];
+                sortingArray[i, max] = temp;
+            }
+        }
+    }
+}
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+SortingArray (array2d);
+PrintMatrix(array2d);
