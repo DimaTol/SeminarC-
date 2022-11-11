4a89338f9ee2d07b2687e455cccb830e5966e1a3
DimaTol <d.tolkachenko@ya.ru>
2022-11-11 15:24:43 +0300

    вывод построчно трехмерного массива с индексом каждого элемента

---
+++ Task60/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int row, int columns, int depth, int min, int max)
+{
+    var matrix = new int[row, columns, depth];
+    var number = new Random().Next(10, 99);
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            matrix[i, j, k] = number;
+            number+=1;
+        }
+    }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k], 4} ({i}, {j}, {k})");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
+int[,,] array3d = CreateMatrixRndInt(2, 2, 2, 1, 10);
+PrintArray(array3d);
