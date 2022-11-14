a35592c4fc0d1c094267d3504b65af29496baf6c
DimaTol <d.tolkachenko@ya.ru>
2022-11-14 12:28:00 +0300

    Рекурсия. Сумма цифр на вход числа

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int SummNumbers (int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SummNumbers (num/10);
+}
+
+int summ = SummNumbers (number);
+Console.WriteLine(summ);
