043ad09b5be406398086df03b7f73cd60bd99e9c
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-11-14 18:28:08 +0300

    Рекурсия. Найти сумму элементов в промежутке от М до N

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,8 +1,7 @@
-﻿// // Задача 66: Задайте значения M и N. Напишите программу, 
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
-
-// // M = 1; N = 15 -> 120
-// // M = 4; N = 8. -> 30
+//  M = 1; N = 15 -> 120
+//  M = 4; N = 8. -> 30
 
 Console.WriteLine("Введите целое число: ");
 int numberM = Convert.ToInt32 (Console.ReadLine());
@@ -12,25 +11,18 @@ int numberN = Convert.ToInt32 (Console.ReadLine());
 
 int SummElements (int numM, int numN)
 {
-    
     int summ = default;
-        
         if (numM <= numN)
         return summ = summ + numM + SummElements(numM+1, numN);
-
         else return summ;
-
 }
-
-if (numberN <= numberM)
-{
-int summ = SummElements (numberN, numberM);
-
-Console.WriteLine(summ);
-}
-
-else
-{
 int summ1 = SummElements (numberM, numberN);
-Console.WriteLine(summ1);
+int summ2 = SummElements (numberN, numberM);
+
+if (numberM <= numberN)
+{
+Console.WriteLine($"M = {numberM}; N = {numberN} -> {summ1}");
 }
+else
+Console.WriteLine($"M = {numberN}; N = {numberM} -> {summ2}");
+
