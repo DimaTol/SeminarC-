e5a848146ad229ab3d7420b33e1555d1ac0f8e20
DimaTol <d.tolkachenko@ya.ru>
2022-11-15 14:24:58 +0300

    Функция Аккермана

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -15,11 +15,10 @@ int Akkerman(int m, int n)
     {
         return n + 1;
     }
-    else if (n == 0 && m > 0)
+    if (n == 0 && m > 0)
     {
         return Akkerman(m - 1, 1);
     }
-    else
     {
         return (Akkerman(m - 1, Akkerman(m, n - 1)));
     }
