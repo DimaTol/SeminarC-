36a489301204dc3d6ca4ed9a74738f66b7314c54
DimaTol <d.tolkachenko@ya.ru>
2022-11-16 13:01:49 +0300

    квадрат числа

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,6 +1,6 @@
-﻿
+﻿// Задача №1. Найти квадрат числа
+
 Console.WriteLine("Введите целое число:  ");
 int number = Convert.ToInt32 (Console.ReadLine());
 int square = number*number;
 Console.WriteLine ($"Квадрат числа {number} = {square}");
-    
