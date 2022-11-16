df2115532957bca387db85f270276bc00888408c
DimaTol <d.tolkachenko@ya.ru>
2022-11-16 12:59:49 +0300

    квадрат числа

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Введите целое число:  ");
 int number = Convert.ToInt32 (Console.ReadLine());
 int square = number*number;
 Console.WriteLine ($"Квадрат числа {number} = {square}");
+    
